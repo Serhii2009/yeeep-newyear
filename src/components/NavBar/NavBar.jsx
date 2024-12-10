6815371f03e8d582334aa841e0ba5bc8074c4f5b
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-12-10 11:42:01 +0200

    created info for each li

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,61 @@
 import './NavBar.css'
+import { useState } from 'react'
 import { assets } from '../../assets/assets'
 import screenfull from 'screenfull'
 
 const NavBar = () => {
+  const [hoveredItem, setHoveredItem] = useState(null)
+
   const toggleFullscreen = () => {
     if (screenfull.isEnabled) {
       screenfull.toggle()
     }
   }
 
+  const navItems = [
+    {
+      label: 'About us',
+      info: 'We created this site to evoke great emotions. Together, let’s make this New Year unforgettable!',
+    },
+    {
+      label: 'Your mission',
+      info: 'Your mission is simple: share this site’s link with as many people as you can. Let’s spread the joy!',
+    },
+    {
+      label: 'Leave the comment',
+      info: 'Share your thoughts, feedback, or a special wish for the New Year. We’d love to hear from you!',
+    },
+    {
+      label: 'Creator',
+      name: 'Serhii Kravchenko',
+    },
+  ]
+
   return (
     <div className="nav-bar">
       <div className="nav-bar-main">
-        <h1 className="nav-bar-logo">YEEEP! IT&apos;S 2025 </h1>
+        <h1 className="nav-bar-logo">YEEEP! IT&apos;S 2025</h1>
 
         <ul className="nav-bar-list">
-          <li>About us</li>
-          <li>Your mission</li>
-          <li>Leave the comment</li>
-          <li>Creator</li>
+          {navItems.map((item, index) => (
+            <li
+              key={index}
+              onMouseEnter={() => setHoveredItem(index)}
+              onMouseLeave={() => setHoveredItem(null)}
+              className="nav-bar-item"
+            >
+              {item.label}
+              {hoveredItem === index && (
+                <div className="nav-bar-tooltip">
+                  <span className="tooltip-arrow"></span>
+                  <div className="tooltip-content">
+                    {item.name}
+                    {item.info}
+                  </div>
+                </div>
+              )}
+            </li>
+          ))}
         </ul>
 
         <div className="nav-bar-mobile-icons">
